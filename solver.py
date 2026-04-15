eed5d8f213f2a56d715a5eb4f963aa009aea1223
nicholas-voigt <nicovo215@googlemail.com>
2026-04-15 17:08:11 +0200

    renaming and clarification

--- solver.py
+++ solver.py
@@ -1,12 +1,13 @@
 ### Wrapper to handle the solving of a sudoku graph with different strategies.
 
 from sudoku_graph import SudokuGraph
-from strategies import brute_force
+from strategies import backtracking_solver
 
 
-def solve_sudoku(sudoku: SudokuGraph, method: str = 'brute_force') -> bool:
-    if method == 'brute_force':
+def solve_sudoku(sudoku: SudokuGraph, method: str = 'standard') -> bool:
+    if method == 'standard':
         cells_to_fill = [node for node in sudoku.nodes.values() if node.value == 0]
-        return brute_force(cells_to_fill)
+        return backtracking_solver(cells_to_fill)
+    
     else:
         raise ValueError(f"Unknown solving method: {method}")
--- strategies.py
+++ strategies.py
@@ -1,17 +1,17 @@
 from sudoku_graph import SudokuGraphNode
 
 
-def brute_force(nodes: list[SudokuGraphNode], idx: int = 0) -> bool:
-    """Brute-force backtracking solver."""    
+def backtracking_solver(nodes: list[SudokuGraphNode], idx: int = 0) -> bool:
+    """Backtracking solver which tries only currently possible values based on the neighboring values."""    
     if idx == len(nodes):
         return True  # Solved
 
     node = nodes[idx]
 
     for num in node.range:
-        if node.check_value(num):
+        if node.check_value(num):  # Try only values that are not already used by neighbors
             node.set_value(num)
-            if brute_force(nodes, idx + 1):  # Recur with the remaining nodes
+            if backtracking_solver(nodes, idx + 1):  # Recur with the remaining nodes
                 return True
             node.set_value(0)  # Backtrack
     
